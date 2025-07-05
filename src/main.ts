20e2a02212fafa4929012f4e1447c4f5a1c6b5d0
Ako-Mawlood <ako.mawlood01@gmail.com>
2025-07-06 00:09:08 +0300

    Add @mdi/font and import Material Design Icons

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { vuetify } from '@/plugins/vuetify'
 import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
 import App from '@/App.vue'
 import router from '@/router'
 
