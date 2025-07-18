b3272eba03206b9d36edfbc859870da71ef2cbe2
Ako-Mawlood <ako.mawlood01@gmail.com>
2025-07-18 23:13:07 +0300

    fix: kurdish font does not apply for some vuetify components

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <script setup lang="ts">
 import { useLanguage } from '@/composables/useLanguage'
+import { watch } from 'vue'
+
 const { locale } = useLanguage()
+
+watch(
+  () => locale.value,
+  (newLocale) => {
+    if (newLocale === 'ku') {
+      document.body.classList.add('kurdish-font')
+    } else {
+      document.body.classList.remove('kurdish-font')
+    }
+  },
+)
 </script>
 
 <template>
-  <v-app :class="{ 'kurdish-font': locale === 'ku' }">
+  <v-app>
     <router-view />
   </v-app>
 </template>
