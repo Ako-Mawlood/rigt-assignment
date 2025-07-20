8e4aec0cc0defe90af818c3524dd9f788e6760d1
Ako-Mawlood <ako.mawlood01@gmail.com>
2025-07-20 08:33:59 +0300

    feat: add position and date of birth fields in members form

---
+++ src/features/dashboard/pages/members/utils/dateFormatter.ts
@@ -0,0 +1,13 @@
+export function formatDateForJsonServer(input: string | Date): string {
+  const date = new Date(input)
+
+  if (isNaN(date.getTime())) {
+    throw new Error('Invalid date')
+  }
+
+  const year = date.getFullYear()
+  const month = String(date.getMonth() + 1).padStart(2, '0')
+  const day = String(date.getDate()).padStart(2, '0')
+
+  return `${year}-${month}-${day}`
+}
