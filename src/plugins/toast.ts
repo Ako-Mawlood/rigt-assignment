8986ade6cf515ac6acb90e7d1c453f567aef6c3e
Ako-Mawlood <ako.mawlood01@gmail.com>
2025-07-26 18:46:02 +0300

    chore: decrease toast timing

--- src/plugins/toast.ts
+++ src/plugins/toast.ts
@@ -1,7 +1,9 @@
 import Toast, { POSITION, type PluginOptions } from 'vue-toastification'
+
 const toastOptions: PluginOptions = {
   position: POSITION.BOTTOM_RIGHT,
   showCloseButtonOnHover: true,
+  timeout: 2000,
   hideProgressBar: true,
   icon: true,
 }
